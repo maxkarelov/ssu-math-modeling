f4b5ff8e9fb4a9185680b8f2d0d97c02adc24079
wlad031 <wlad03131@gmail.com>
2015-11-03 15:15:50 +0300

    Seidel algorithm added (tridigonal matrix algorithm has bugs)

--- matrix.py
+++ matrix.py
@@ -53,6 +53,43 @@ def determinant(matrix):
 ## Defining funcions for solving the systems of linear equations ##
 ###################################################################
 
+# Tridiagonal matrix algorithm
+def tridiagonal(A, B):
+    n = len(A)
+
+    data = [A[i] + [B[i]] for i in range(n)]
+    data = triangular(data)
+    data = [normalize_list(data[i], key=i) for i in range(n)]
+
+    x = range(n) # will be edited and returned
+    P = Q = alpha = beta = gamma = delta = range(n)
+
+    for i in range(n):
+        if i == 0:
+            alpha[i] = 0
+        elif i < n - 1:
+            alpha[i] = data[i + 1][i]
+
+        beta[i] = -data[i][i]
+        gamma[i] = data[i][i + 1]
+        delta[i] = data[i][n]
+
+    P[0] = gamma[0] / beta[0]
+    Q[0] = -delta[0] / beta[0]
+
+    for i in range(1, n):
+        P[i] = gamma[i] / (beta[i] - alpha[i] * P[i - 1])
+        Q[i] = (alpha[i] * Q[i - 1] - delta[i]) / \
+               (beta[i] - alpha[i] * P[i - 1])
+
+    x[n - 1] = (alpha[n - 1] * Q[n - 2] - delta[n - 1]) / \
+               (beta[n - 1] - alpha[n - 1] * P[n - 2])
+
+    for i in range(n - 2, -1, -1):
+        x[i] = P[i] * x[i + 1] + Q[i]
+
+    return x    
+
 # Gauss' algorithm
 def gauss(A, B):
     n = len(A)
@@ -75,7 +112,7 @@ def gauss(A, B):
 # Yacobi's algorithm
 def yacobi(A, B, eps=1.0e-4):
 
-    def yacobi_iter(A, B, X, k):
+    def yacobi_iter(X, k):
         n = len(A)
 
         # Uncomment line below for the checking variable k
@@ -97,11 +134,44 @@ def yacobi(A, B, eps=1.0e-4):
                 need_for_new_iteration = True
 
         if need_for_new_iteration:
-            return yacobi_iter(A, B, Xnew, k + 1)
+            return yacobi_iter(Xnew, k + 1)
         else:
             return Xnew
 
-    return yacobi_iter(A, B, [0] * len(A), 0)
+    return yacobi_iter([0] * len(A), 0)
+
+# Seidel-Gauss algo
+def seidel(A, B, eps=1.0e-4):
+
+    def seidel_iter(X, k):
+        n = len(A)
+
+        # Uncomment line below for the checking variable k
+        # print k
+
+        Xnew = range(n)
+        need_for_new_iteration = False
+
+        for i in range(n):
+            s1 = s2 = 0
+
+            for j in range(n):
+                if j < i:
+                    s1 += A[i][j] * Xnew[j]
+                elif j > i:
+                    s2 += A[i][j] * X[j]
+
+            Xnew[i] = 1 / A[i][i] * (B[i] - s1 - s2)
+
+            if abs(Xnew[i] - X[i]) > eps:
+                need_for_new_iteration = True
+
+        if need_for_new_iteration:
+            return seidel_iter(Xnew, k + 1)
+        else:
+            return Xnew
+
+    return seidel_iter([0] * len(A), 0)
 
 
 #######################################
@@ -116,6 +186,12 @@ if __name__ == "__main__":
 
     A = [[lv[i] if i == j else le[i] for j in range(6)] for i in range(6)]
     B = [18, 38.88, 67.68, 104.4, 149.04, 201.6]
+
+    # Tridiagonal matrix
+    #  for i in range(len(A)):
+    #     for j in range(len(A)):
+    #        if abs(i - j) > 1:
+    #            A[i][j] = 0
 
     print "Matrix:"
     print_table(data=A, horiz_sep=True, vert_sep=True)
@@ -132,8 +208,17 @@ if __name__ == "__main__":
         print
 
     if "sle" in sys.argv:
-        # ans = gauss(A, B)
-        ans = yacobi(A, B)
+        if "gauss" in sys.argv:
+            ans = gauss(A, B)
+        elif "yacobi" in sys.argv:
+            ans = yacobi(A, B)
+        elif "seidel" in sys.argv:
+            ans = seidel(A, B)
+        elif "tridiagonal" in sys.argv: 
+            ans = tridiagonal(A, B)
+        else:
+            print "Sorry, I don't know this algorithm"
+            exit(0)
 
         print "Column B:"
         print_table(data=rotate_2D_list([B]), horiz_sep=True, vert_sep=True, \
